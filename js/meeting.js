58ef5d4c540f9aa64a56d880021a2e3db30ef5b3
jc4va <31323449+jc4va@users.noreply.github.com>
2019-04-10 19:49:37 -0400

    dynamically generate the table with times and dates

--- js/meeting.js
+++ js/meeting.js
@@ -1,17 +1,30 @@
 function showInput() {
+
+    // var cols = hidden value input from php (difference between dates)
+    var cols = document.getElementById("diff").value;
+
     // document.getElementById('display').innerHTML = document.getElementById("user_input").value;
 
-    // var rows = document.getElementById("rows").value;
-    // var cols = document.getElementById("cols").value;
-    // var rows = document.getElementById('row').value;
+    var day1 = document.getElementById("row").value;
+    var day2 = document.getElementById("col").value;
+
     var rows = '16';
-    var cols = document.getElementById('col').value;
     console.log(rows, cols);
 
     // var x = document.createElement("TABLE");
     // x.setAttribute("id", "myTable");
     var x = document.getElementById('myTable');
+    
+    // Create table header
+    for (i = day1; i <= day2; i++){
+        var d = document.createTextNode(i);
+        var z = document.createElement("TD");
+        z.setAttribute("id", "header");
+        z.appendChild(d);
+        document.getElementById("myTable").appendChild(z);
+    }
 
+    // Populate the table with 
     var timeArray = ['8:00AM','9:00AM','10:00AM','11:00AM','12:00PM','1:00PM','2:00PM','3:00PM','4:00PM','5:00PM','6:00PM','7:00PM','8:00PM','9:00PM','10:00PM','11:00PM','12:00AM'];
     for (i = 0; i < rows; i++) {
         var y = document.createElement("TR");
@@ -19,9 +32,10 @@ function showInput() {
         document.getElementById("myTable").appendChild(y);
         for (j = 0; j < cols; j++) {
             var z = document.createElement("TD");
-            var t = document.createTextNode("8:00");
+            var t = document.createTextNode(timeArray[i]);
             z.appendChild(t);
             document.getElementById("myTr"+ String(i)).appendChild(z);
         }
     }
+
 }
--- meeting.php
+++ meeting.php
@@ -34,13 +34,15 @@ session_start();
             <p><span id='display'></span></p>
             <table id='myTable'></table>
         </div>
-        <div class='right-side card'>
-        </div>
     </div>
 
     <!-- To read data from session variables -->
     <input id='row' value='<?php echo $_SESSION['$date1_day']; ?>'/>
     <input id='col' value='<?php echo $_SESSION['$date2_day']; ?>'/>
+
+    <!-- This will pass the number of dates (number of columns in the table) to javascript as a hidden value -->
+    <input type="hidden" id="diff" name="diff" value='<?php echo $_SESSION['$date2_day'] - $_SESSION['$date1_day'] + 1; ?>'/>
+
 <!-- 
     <input type="submit" onclick="showInput()"><br />
     <label>Your input: </label> -->
