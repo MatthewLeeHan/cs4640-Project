efecf683827e7b4e751165b396a4cfc933649a1a
jc4va <31323449+jc4va@users.noreply.github.com>
2019-04-09 22:29:06 -0400

    incomplete login

--- php/login.php
+++ php/login.php
@@ -8,10 +8,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["user_id"];
     $pwd = md5($_POST["password"]);
 
-    $query = "SELECT username FROM user WHERE username='$username' AND password='$pwd'";
+    $query = "SELECT username FROM user WHERE username=':username' AND password=':pwd'";
     $statement = $db->prepare($query);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':pwd', $pwd);
     $statement->execute();
 
+    $results = $statement->fetchAll();
+    if (count($results) == 1) {
+        $_SESSION['username'] = $username;
+
+        header('Location: createEvent.php');
+    }
+
+    else{
+        header('Location: ../login.html');
+    }
 
 }
 
