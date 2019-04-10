af253b8269b7b82154895d63be56279473d6219d
jc4va <31323449+jc4va@users.noreply.github.com>
2019-04-09 21:42:48 -0400

    signup without hashing pw

--- php/signup.php
+++ php/signup.php
@@ -1,29 +1,33 @@
 <?php
-
     session_start();
 
     if (isset($_POST['submit'])){
+
+        require('connect-db.php');
 
         $username = $_POST['user_id'];
         $password = $_POST['password'];
         $name = $_POST['name'];
         $email = $_POST['email'];
 
-        header('Location: http://localhost/cs4640-project/createEvent.html');
+        $query = "INSERT INTO user VALUES(:username, :password, :name, :email)";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $username);
+        $statement->bindValue(':password', $password);
+        $statement->bindValue(':name', $name);
+        $statement->bindValue(':email', $email);
+            
+        $statement->execute();
+        $statement->closeCursor();
+
+        header("Location: http://localhost/cs4640-project/");
+        exit();
     }
     else{
-        header('Location: http://localhost/cs4640-project/');  
+        header("Location: http://localhost/cs4640-project/signup.html");
+        exit();
     }
 
     /*
-    $query = "INSERT INTO user VALUES(:username, :password, :name, :email)";
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username', $username);
-    $statement->bindValue(':password', $password);
-    $statement->bindValue(':name', $name);
-    $statement->bindValue(':email', $email);
-        
-    $statement->execute();
-    $statement->closeCursor();
     */
 ?>
