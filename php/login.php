0af17c1ea814cdad9156b66dffcd63652389ba5d
jc4va <31323449+jc4va@users.noreply.github.com>
2019-04-15 14:38:30 -0400

    Changing password function implemented

--- php/login.php
+++ php/login.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                 }
                 else{
-                    header('Location: ../logIn.php');
+                    header('Location: ./logIn.php');
                 }
             }
         }
