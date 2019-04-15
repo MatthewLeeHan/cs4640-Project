f19cbd254f0d71b1b999c334861786d15499ae9d
jc4va <31323449+jc4va@users.noreply.github.com>
2019-04-15 19:29:22 -0400

    added cookie-not-set handler

--- php/account-settings.php
+++ php/account-settings.php
@@ -16,44 +16,48 @@ function test_input($data) {
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $username = $_COOKIE['user'];
-    $pwd = md5(test_input($_POST['old_pw']));
-
-    $query = "SELECT * FROM user WHERE username = :username";
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username', $username);
-    $statement->execute();
-
-    $results = $statement->fetchAll();
-
-    if (empty($results)){
-        // header('Location: ../logIn.php');
+    if (!isset($_COOKIE['user'])){
+        echo "<script>alert('Please log in first!'); window.location.href='../login.php';</script>";
     }
+
     else{
-        foreach ($results as $result){
-            $db_hash = $result['password'];
-            if (count($results) == 1) {
-                if ($pwd == $db_hash){
-                    if ($_POST['new_pw'] == $_POST['new_pw_confirm']){
-                        $password = md5($_POST['new_pw']);
-                        $update_query = "UPDATE user SET password=:password WHERE username=:username";
-                        $statement = $db->prepare($update_query);
-                        $statement->bindValue(':username', $username);
-                        $statement->bindValue(':password', $password);
-                        $statement->execute();
-                        $statement->closeCursor();   
+        
+        $username = $_COOKIE['user'];
+        $pwd = md5(test_input($_POST['old_pw']));
 
-                        echo '<script type="text/javascript">alert("Password successfully changed")</script>';
-                        header("Location: ../index.html");
+        $query = "SELECT * FROM user WHERE username = :username";
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $username);
+        $statement->execute();
 
-                    }
+        $results = $statement->fetchAll();
+
+        if(empty($results)){
+            foreach ($results as $result){
+                $db_hash = $result['password'];
+                if (count($results) == 1) {
+                    if ($pwd == $db_hash){
+                        if ($_POST['new_pw'] == $_POST['new_pw_confirm']){
+                            $password = md5($_POST['new_pw']);
+                            $update_query = "UPDATE user SET password=:password WHERE username=:username";
+                            $statement = $db->prepare($update_query);
+                            $statement->bindValue(':username', $username);
+                            $statement->bindValue(':password', $password);
+                            $statement->execute();
+                            $statement->closeCursor();   
+
+                            echo '<script type="text/javascript">alert("Password successfully changed")</script>';
+                            header("Location: ../index.html");
+
+                        }
+                }
+                else{
+                    header("Location: ../account-settings.html");
+                }
             }
             else{
                 header("Location: ../account-settings.html");
             }
-        }
-        else{
-            header("Location: ../account-settings.html");
         }
     }
 }
