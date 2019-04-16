cf828d5c87307718bb32bb1596c73600b2455310
MatthewLeeHan <mlh6fc@virginia.edu>
2019-04-15 22:14:21 -0400

    get request

---
+++ help2.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Help (FAQ)</title>
+    <!-- <link rel="stylesheet" href="./css/help.css"/> -->
+    <link rel="stylesheet" href="./css/help2.css"/>
+</head>
+<body>
+<div class="containerHeader">
+		<header>
+			<div class="logo">
+				<h1><a href = "index.html"> ScheduleMe </a></h1>
+            </div> 
+        </header>
+    </div>
+    <div id="pageContent">
+        <div class='container'>
+            <h2>Hello <?php echo $_GET['uname']?>!</h2>
+            <div class='help-text'>
+                <h3>How does ScheduleMe work?</h3>
+                <p>You can create an account and create your own ScheduleMe Meeting event. You can then send out a mass email to all meeting attendees to provide their availabilities in order to find a common time to meet. Meeting attendees do not require accounts to reply to your ScheduleMe meeting invite.</p>
+                <h3>Who created ScheduleMe?</h3>
+                <p>Jiwon Cha and Matthew Han, third-years at the University of Virginia</p>
+                <h3>Is it free?</h3>
+                <p>Yes.</p>
+
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
